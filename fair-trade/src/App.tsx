af46f7098b25baf2938f232087317ec8e39ee0d9
Lawrence Tong <lawrencetong1030@gmail.com>
2026-05-12 13:20:11 -0500

    feat: initial commit

---
+++ fair-trade/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <main className="hero">
+        <h1 className="hero__heading">The House doesn't win here, you do</h1>
+        <h2 className="hero__subheading">Welcome to Fair Trade</h2>
+        <button className="btn-primary">Join Now</button>
+
+        <nav></nav>
+      </main>
+    </>
+  )
+}
+
+export default App
